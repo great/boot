ac45b428b47d8f61dcc9fb9f55261024916495b8
Intae Kim <greatkit@gmail.com>
2019-12-03 21:56:48 +0900

    hello world example

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,9 +2,15 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
+	@GetMapping
+	fun hello() = "Hello, World!"
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
